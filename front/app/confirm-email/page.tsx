8a24e38736d2107336a512940f45111cfbe1f703
KCsam11 <samquibel@icloud.com>
2025-06-20 23:23:22 +0200

    fix: update vercel configuration for nextjs

--- front/app/confirm-email/page.tsx
+++ front/app/confirm-email/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-
+import { Suspense } from 'react';
 import { ConfirmEmailDialog } from '@/components/ui/confirm-email-dialog';
 
 export default function ConfirmEmailPage() {
   return (
-    <div className='flex items-center justify-center min-h-screen bg-background'>
+    // Enveloppez le composant qui utilise useSearchParams avec Suspense
+    <Suspense fallback={<div>Chargement...</div>}>
       <ConfirmEmailDialog />
-    </div>
+    </Suspense>
   );
 }
